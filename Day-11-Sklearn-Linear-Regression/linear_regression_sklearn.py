54e9d28b1968c9e3ec58a7ee645cad49161b1744
AbdulRehman028 <47baig@gmail.com>
2025-09-19 21:10:56 +0500

    for training step in linear_regression_sklearn.py remove comments

--- Day-11-Sklearn-Linear-Regression/linear_regression_sklearn.py
+++ Day-11-Sklearn-Linear-Regression/linear_regression_sklearn.py
@@ -19,15 +19,9 @@ except FileNotFoundError:
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# ------------------------
-# Step 3: Train Linear Regression Model
-# ------------------------
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-# ------------------------
-# Step 4: Predictions & Evaluation
-# ------------------------
 y_pred = model.predict(X_test)
 
 mse = mean_squared_error(y_test, y_pred)

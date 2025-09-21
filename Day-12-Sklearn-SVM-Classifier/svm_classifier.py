75a1ae80c39490f1bf005769210e2f3fe49ef914
AbdulRehman028 <47baig@gmail.com>
2025-09-21 23:07:06 +0500

    Remove redundant comment for train/test split step in svm_classifier.py

--- Day-12-Sklearn-SVM-Classifier/svm_classifier.py
+++ Day-12-Sklearn-SVM-Classifier/svm_classifier.py
@@ -13,7 +13,6 @@ iris = datasets.load_iris()
 X = iris.data[:, :2]  # Use first two features for visualization (Sepal length & width)
 y = iris.target
 
-# Step 2: Train/Test Split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
 

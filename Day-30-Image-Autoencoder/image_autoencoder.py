a3289c97841dbff710c3f510d32536ef7740b29b
AbdulRehman028 <47baig@gmail.com>
2025-10-31 18:50:58 +0500

    fixes

--- Day-30-Image-Autoencoder/image_autoencoder.py
+++ Day-30-Image-Autoencoder/image_autoencoder.py
@@ -7,7 +7,6 @@ from tensorflow.keras.optimizers import Adam
 
 # ===============================
 # 1. Load and preprocess data
-# ===============================
 (x_train, _), (x_test, _) = mnist.load_data()
 x_train = x_train.astype("float32") / 255.0
 x_test = x_test.astype("float32") / 255.0

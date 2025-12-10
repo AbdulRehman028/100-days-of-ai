ef73334b667fd583c58ec76678258b2f75ef4817
AbdulRehman028 <47baig@gmail.com>
2025-12-10 22:40:43 +0500

    add comment to clarify the purpose of the HuggingFace Router API

--- Day-37-Meme-Caption-Generator/app.py
+++ Day-37-Meme-Caption-Generator/app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB max file size
 
+# HuggingFace Router API
 API_URL = "https://router.huggingface.co/v1/chat/completions"
 API_TOKEN = os.getenv("HF_API_TOKEN", "")
 
